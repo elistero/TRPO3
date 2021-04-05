49b0017089bb0c10f260cda80df462869b46b3a8
”elsa <e.krauskaite@gmail.com>
2021-04-05 14:45:21 +0300

    lab4

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
 <?php
-namespace Laba3;
 
 use Krauskeite\MyLog;
+use Krauskeite\SuslovException;
+use Krauskeite\QuEquation;
 
-require_once "core\EquationInterface.php";
-require_once "core\LogAbstract.php";
-require_once "core\LogInterface.php";
-require_once "Krauskeite\Line.php";
-require_once "Krauskeite\MyLog.php";
-require_once "Krauskeite\Quadratic.php";
-MyLog::log("elsa");
+include "core/EquationInterface.php";
+include "core/LogAbstract.php";
+include "core/LogInterface.php";
+include "Krauskeite/MyLog.php";
+include "Krauskeite/Equation.php";
+include "Krauskeite/QuEquation.php";
+include "Krauskeite/KrauskeiteException.php";
+
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+try {
+    $b = new QuEquation();
+    $values = array();
+
+    for ($i = 1; $i < 4; $i++) {
+        echo "Введите " . $i . " аргумент: ";
+        $values[] = readline();
+    }
+    $va = $values[0];
+    $vb = $values[1];
+    $vc = $values[2];
+
+    MyLog::log("Введено уравнение " . $va . "x^2 + " . $vb . "x + " . $vc . " = 0");
+    $x = $b->solve($va, $vb, $vc);
+
+    $str = implode(", ", $x);
+    MyLog::log("Корни уравнения: " . $str);
+} catch (KrauskeiteException $e) {
+    MyLog::log($e->getMessage());
+}
+
 MyLog::write();
+
+?>
