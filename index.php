b24a7348ae19b947deee4d9931f3369d832d7ea6
”elsa <e.krauskaite@gmail.com>
2021-04-05 14:40:12 +0300

    lab2

--- index.php
+++ index.php
@@ -1,34 +1,43 @@
 <?php
 
-class A {
+class A
+{
+    protected $x;
 
+    public function line_equation($a, $b)
+    {
+        if ($a != 0) {
+            return $this->x = (-$b) / $a;
+        }
+        return null;
+    }
 }
 
-class B extends A {
-	public function __construct($a, $b, $c, $d) {
-		$this->a = $a;
-		$this->b = $b;
-		$this->c = $c;
-		$this->d = $d;
-	}
-	protected $a;
-	protected $b;
-	protected $c;
-	protected $d;
+class B extends A
+{
+    public function disclamer($a, $b, $c)
+    {
+        return ($b * $b) - 4 * $a * $c;
+    }
+
+    public function quadratic_equation($a, $b, $c)
+    {
+        If ($a === 0) {
+            return parent::line_equation($b, $c);
+        }
+        $d = $this->disclamer($a, $b, $c);
+        if ($d > 0) {
+            $eg = sqrt($d);
+            return $this->x = array((-$b - $eg) / (2 * $a), (-$b + $eg) / (2 * $a));
+        }
+        if ($d === 0) {
+            return $this->x = array(-$b / (2 * $a));
+        }
+        return null;
+    }
 }
 
-$a1 = new A();
-$a2 = new A();
-$a3 = new A();
-$a4 = new A();
-$b5 = new B($a1, $a2, $a3, $a4);
-
-var_dump($b5);
-
-$b5 = new B( new A(), new A(), new A(), new A());
-
-echo "<br>";
-
-var_dump($b5);
-
-?>
+$aclass = new A();
+print_r($aclass->line_equation(4, 5));
+$bclass = new B();
+print_r($bclass->quadratic_equation(0, 6, 7));
